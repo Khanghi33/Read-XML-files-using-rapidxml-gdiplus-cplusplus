136d134315b0d79074cf3ca52e9b1c5c904e7c4a
hyn69 <hoangamerminecraft@gmail.com>
2024-12-31 11:37:13 +0700

    Add Code Circle

--- SVGDemo/SVGDemo/SVGDemo/Circle.h
+++ SVGDemo/SVGDemo/SVGDemo/Circle.h
@@ -4,13 +4,18 @@ using namespace std;
 using namespace rapidxml;
 using namespace Gdiplus;
 
+// The CIRCLE class inherits from the Shape class and represents a circle with attributes such as center coordinates, radius, and styling properties.
+// It provides constructors for initializing a circle with explicit parameters or parsing data from an XML node, and a method for rendering the circle.
+
 class CIRCLE : public Shape {
 private:
+	// - cx, cy: Represent the x and y coordinates of the circle's center.
+	// - r: Represents the radius of the circle.
 	REAL cx, cy, r;
 public:
-	CIRCLE();
-	CIRCLE(int cx, int cy, int r, string stroke, string fill, string stroke_width, string fill_opactiy, string stroke_opacity);
-	CIRCLE(xml_node<>* node);
-	~CIRCLE();
-	VOID Draw(HDC hdc);
+	CIRCLE(); //Default constructor initializes the circle with default values.
+	CIRCLE(int cx, int cy, int r, string stroke, string fill, string stroke_width, string fill_opactiy, string stroke_opacity); //   Initializes the circle with specific coordinates, radius, and styling attributes.
+	CIRCLE(xml_node<>* node);// Parses circle attributes from an XML node, enabling deserialization.
+	~CIRCLE(); // Cleans up resources if necessary (not explicitly detailed in the provided code but included for proper lifecycle management).
+	VOID Draw(HDC hdc); //Draws the circle on the provided device context(HDC) using its attributes for position, size, and style.
 };
