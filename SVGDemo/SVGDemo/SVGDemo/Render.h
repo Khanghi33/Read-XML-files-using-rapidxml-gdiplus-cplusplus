312b1d0688e9bde0740865166d52a38e64521b96
chullzz <superdogdanh9912@gmail.com>
2024-12-31 15:27:28 +0700

    comment render

--- SVGDemo/SVGDemo/SVGDemo/Render.h
+++ SVGDemo/SVGDemo/SVGDemo/Render.h
@@ -3,7 +3,6 @@
 using namespace std;
 using namespace rapidxml;
 using namespace Gdiplus;
-//class render use mutiple shape and function render to optimize shape
 class Render {
 private:
 	vector<Shape*> shapes;
@@ -12,3 +11,4 @@ public:
 	Render(vector<Shape*> shapes, HDC* hdc);
 	void GDIRender();
 };
+//class render use mutiple shape and function render to optimize shape
